90a9d0cefcde413eb24dea4d85e5db677fb9154b
Ren2025 <rhancock2025@outlook.com>
2024-09-15 21:07:29 +0100

    update

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-document.getElementById('searchform').onsubmit = function() {
-    window.location = 'http://www.google.com/search?q=site:ren2025.github.io ' + document.getElementById('test').value;
-    return false;
-}
-
 function myFunction() {
-    var x = document.getElementById("mytopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("mytopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
