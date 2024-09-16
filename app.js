fa70ad09ef60c52b7ffcd1bc4ae6c2694b1974fa
Ren2025 <rhancock2025@outlook.com>
2024-09-16 01:17:14 +0100

    Update dark mode

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ function myFunction() {
   }
 }
 
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+//function myFunction() {
+//  var element = document.body;
+//  element.classList.toggle("dark-mode");
+//}
+
+//let mode;
+//mode = localStorage.getItem('mode');
+
+//if (mode === 'light'){
+//  lightMode();
+//}else{
+//  darkMode();
+//}
+
+function darkMode() {
+  const wasdarkMode = localStorage.getItem('darkMode') === 'true';
+  localStorage.setItem('darkMode', !wasdarkMode);
+  const element = document.body;
+  element.classList.toggle('dark-mode', !wasdarkMode);
+}
+function onload() {
+  document.body.classList.toggle('dark-mode', localStorage.getItem('darkMode') === 'true');
 }
